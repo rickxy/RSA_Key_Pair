8b4c33e6b2b4f97cbd17a66e65038fab5c552cb7
NetGo+ Dev <36708000+rickxy@users.noreply.github.com>
2022-12-14 13:49:07 +0300

    Create keygen.py
    upload

---
+++ keygen.py
@@ -0,0 +1,86 @@
+from bs4 import BeautifulSoup
+from math import sqrt;
+from itertools import count, islice
+import random
+import requests
+
+def isPrime(n):
+    return n > 1 and all(n%i for i in islice(count(2), int(sqrt(n)-1)))
+
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def multiplicative_inverse(a, b):
+    x = 0
+    y = 1
+    lx = 1
+    ly = 0
+    oa = a
+    ob = b
+    while b != 0:
+        q = a // b
+        (a, b) = (b, a % b)
+        (x, lx) = ((lx - (q * x)), x)
+        (y, ly) = ((ly - (q * y)), y)
+    if lx < 0:
+        lx += ob
+    if ly < 0:
+        ly += oa
+    return lx
+
+
+def is_prime(num):
+    if num == 2:
+        return True
+    if num < 2 or num % 2 == 0:
+        return False
+    for n in xrange(3, int(num ** 0.5) + 2, 2):
+        if num % n == 0:
+            return False
+    return True
+
+
+def generate_keypair(p, q):
+    if not (is_prime(p) and is_prime(q)):
+        raise ValueError('Both numbers must be prime.')
+    elif p == q:
+        raise ValueError('p and q cannot be equal')
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = random.randrange(1, phi)
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+    d = multiplicative_inverse(e, phi)
+    return ((e, n), (d, n))
+
+
+def encrypt(pk, plaintext):
+    key, n = pk
+    cipher = [(ord(char) ** key) % n for char in plaintext]
+    return cipher
+
+
+def decrypt(pk, ciphertext):
+    key, n = pk
+    plain = [chr((char ** key) % n) for char in ciphertext]
+    return ''.join(plain)
+
+r = requests.get('https://www.random.org/integers/?num=100&min=100&max=10000&col=1&base=10&format=plain&rnd=new')
+c = r.text
+soup = BeautifulSoup(c)
+list = soup.prettify()
+primes = []
+for num in list.split():
+    if len(primes) == 2:
+        break
+    if isPrime(int(num)):
+        primes.append(int(num))
+
+public, private = generate_keypair(primes[0],primes[1])
+print public
+print private
